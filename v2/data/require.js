e6749a25f1f8ca40b268ad5503c8f95ea6231385
Hans5958 <email5958.1@gmail.com>
2019-05-17 15:46:07 +0700

    Website upgrade

--- v2/data/require.js
+++ v2/data/require.js
@@ -1,5 +1,5 @@
 $(function(){
-  $("head").append("<style>html{position:relative;min-height:100%}body{margin-bottom:60px}.footer{position:absolute;bottom:0;width:100%;height:60px;line-height:60px;background-color:#f5f5f5}</style>")
+  $("head").append("<style>html{position:relative;min-height:100%}.footer{position:absolute;bottom:0;width:100%;height:3rem;background-color:#f5f5f5;margin-top:3rem}.footer div.text-muted{margin:1rem 0}@media (max-width:575.98px){.footer{height:4rem;margin-top:4rem}.footer div.text-muted{margin:1rem 0}}</style>")
   $("body").prepend("<span id=\"nav\">")
   $("body").append("<span id=\"footer\">")
   $("#nav").load("data/nav.html")
